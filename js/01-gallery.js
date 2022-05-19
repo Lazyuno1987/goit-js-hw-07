8abac7006f9c5301a41edd6dc4a71859e85c7559
Lazyuno1987 <95475727+Lazyuno1987@users.noreply.github.com>
2022-05-19 13:34:20 +0300

    DZ ready

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
 console.log(galleryItems);
+const galeryElement = document.querySelector(".gallery");
+galeryElement.addEventListener("click", onCreateModal);
+galeryElement.addEventListener("keydown", onCloseModal);
+galeryElement.insertAdjacentHTML(
+  "beforeend",
+  oncreateMarkUpGalery(galleryItems)
+);
+
+function oncreateMarkUpGalery(galery) {
+  return galery
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+let instance;
+function onCreateModal(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+  instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600"> `);
+
+  instance.show();
+}
+
+function onCloseModal(evt) {
+  if (evt.code === "Escape") {
+    instance.close();
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const galeryElement = document.querySelector(".gallery");
+galeryElement.addEventListener("click", onCreateModal);
+galeryElement.insertAdjacentHTML(
+  "beforeend",
+  oncreateMarkUpGalery(galleryItems)
+);
+
+function oncreateMarkUpGalery(galery) {
+  return galery
+    .map(({ preview, original, description }) => {
+      return `<li><a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a></li>`;
+    })
+    .join("");
+}
+
+function onCreateModal(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const lightbox = new SimpleLightbox(".gallery a", { captionsData: "alt" });
+}
